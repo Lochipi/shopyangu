2ed9f56484a8f9f6d5279566ff64e31124d516e5
Emase <corneliuslochipi@gmail.com>
2024-11-30 12:58:07 +0300

    update; modified to allow search feat

--- src/server/api/routers/products/listProducts.ts
+++ src/server/api/routers/products/listProducts.ts
@@ -4,13 +4,44 @@ import { publicProcedure } from "../../trpc";
 export const listProducts = publicProcedure
   .input(
     z.object({
-      shopId: z.string().optional(), // Optional: Filter by shop
+      shopId: z.string().optional(),
+      productName: z.string().optional(),
+      price: z.number().optional(),
+      stockLevel: z.number().optional(),
     }),
   )
   .query(async ({ input, ctx }) => {
     const products = await ctx.db.product.findMany({
       where: {
-        ...(input.shopId && { shopId: input.shopId }),
+        AND: [
+          input.shopId
+            ? {
+                shopId: input.shopId,
+              }
+            : {},
+          input.productName
+            ? {
+                name: {
+                  contains: input.productName,
+                  mode: "insensitive",
+                },
+              }
+            : {},
+          input.price
+            ? {
+                price: {
+                  lte: input.price, // Returns products with price less than or equal to the provided price
+                },
+              }
+            : {},
+          input.stockLevel
+            ? {
+                stockLevel: {
+                  lte: input.stockLevel,
+                },
+              }
+            : {},
+        ],
       },
     });
     return products;
